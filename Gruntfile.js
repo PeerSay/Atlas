6ec2e8a012af6bb94d231c2ae0c72924384cef5a
Pavel Zhytko <pjitco@gmail.com>
2014-09-23 06:17:02 +0300

    add grunt tasks for front-end tests: unit (karma) and e2e (protractor)

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,9 +8,10 @@ module.exports = function (grunt) {
     grunt.initConfig({
         //cfg: grunt.file.readJSON('config.json'),
 
-        // Tasks
+        // Watch - monitors changes and runs tasks
+        //
         watch: {
-            test: {
+            assets: {
                 options: {
                     livereload: 35729,
                     spawn: false
@@ -19,9 +20,16 @@ module.exports = function (grunt) {
                     'Gruntfile.js',
                     'static/**/*.*'
                 ]
+            },
+
+            karma: {
+                files: ['static/js/**/*.js'],
+                tasks: ['karma:unit:run']
             }
         },
 
+        // Connect - runs web server (might be useful, eventually)
+        //
         /*connect: {
             options: {
                 port: 9000,
@@ -39,6 +47,8 @@ module.exports = function (grunt) {
             }
         }*/
 
+        // Tests - server
+        //
         mochaTest: {
             test: {
                 options: {
@@ -46,10 +56,54 @@ module.exports = function (grunt) {
                 },
                 src: ['test/**/*.js']
             }
+        },
+
+        // Tests - client (unit)
+        //
+        karma: {
+            options: {
+                basePath : './static',
+                files: [
+                    'js/test/**/*.js'
+                ],
+                exclude: [
+                    'js/test/e2e/*.*'
+                ],
+                frameworks: ['mocha', 'chai']
+            },
+            // watch mode - runs karma server and lets watch re-run tests
+            unit: {
+                background: true,
+                singleRun: false,
+                browsers: ['Chrome']
+            },
+            // continuous integration mode: run tests once in PhantomJS browser.
+            continuous: {
+                singleRun: true,
+                browsers: ['PhantomJS']
+            }
+        },
+
+        // Tests - client (e2e)
+        //
+        protractor: {
+            options: {
+                configFile: "node_modules/protractor/docs/referenceConf.js", // Default config file
+                keepAlive: false,
+                args: {
+                    /*seleniumAddress: 'http://localhost:4444/wd/hub',*/
+                    specs: ['./static/js/test/e2e/*.js'],
+                    baseUrl: 'http://localhost:5000',
+                    chromeOnly: true,
+                    chromeDriver: 'node_modules/protractor/selenium/chromedriver'
+                }
+            },
+            run: {} // grunt requires at least one task
         }
     });
 
-    grunt.registerTask('test', ['mochaTest']);
-    grunt.registerTask('dev', ['watch']);
+    grunt.registerTask('test', ['mochaTest', 'karma:continuous', 'protractor:run']);
+    grunt.registerTask('dev', ['karma:unit:start', 'watch']); // TODO: jshint, less
+    grunt.registerTask('prod', ['test']); // TODO: dev + uglify, concat/copy, cssmin
     grunt.registerTask('default', ['dev']);
 };

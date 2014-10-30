8f614963d1a042d353c801ec1e18c88dfacc43f8
Pavel Zhytko <pjitco@gmail.com>
2014-10-31 01:13:08 +0300

    add markup/logic for project header, checklist & tiles; some basic logic

--- static/js/projects/projects.ctrl.js
+++ static/js/projects/projects.ctrl.js
@@ -26,12 +26,89 @@ ProjectDetailsCtrl.$inject = ['Projects', '$routeParams'];
 function ProjectDetailsCtrl(Projects, $routeParams) {
     var m = this;
     var id = Number($routeParams.projectId);
+    m.tileView = 'norm';
+    m.tileBtnClass= {
+        'glyphicon-zoom-out': m.tileView === 'norm',
+        'glyphicon-zoom-in': m.tileView === 'min'
+    };
+    m.toggleTileView = toggleTileView;
+    m.tiles = [
+        {
+            name: 'essentials',
+            title: 'Project Essentials',
+            progress: '1/10',
+            show: true,
+            html: '/html/project-essentials.html'
+        },
+        {
+            name: 'evaluation',
+            title: 'Evaluation Requirements',
+            progress: '1/10',
+            show: false,
+            html: '/html/project-todo.html'
+        },
+        {
+            name: 'vendor-input',
+            title: 'Vendor Input',
+            progress: '1/10',
+            show: false,
+            html: '/html/project-todo.html'
+        },
+        {
+            name: 'shortlists',
+            title: 'Shortlists',
+            progress: '1/10',
+            show: false,
+            html: '/html/project-todo.html'
+        },
+        {
+            name: 'pocs',
+            title: 'POCs',
+            progress: '1/10',
+            show: false,
+            html: '/html/project-todo.html'
+        },
+        {
+            name: 'vendor-ref',
+            title: 'Vendor Reference',
+            progress: '1/10',
+            show: false,
+            html: '/html/project-todo.html'
+        },
+        {
+            name: 'debrief',
+            title: 'Audit / Debrief',
+            progress: '1/10',
+            show: false,
+            html: '/html/project-todo.html'
+        }
+    ];
+    m.curTile = m.tiles[0];
+    m.toggleTile = toggleTile;
 
-    Projects
-        .getProjects()
-        .success(function () {
-            m.project = $.map(Projects.projects, function (p) {
-                return (p.id !== id) ? null : p;
-            })[0];
-        });
+    getProject();
+
+    function getProject() {
+        Projects
+            .getProjects()
+            .success(function () {
+                m.project = findBy('id')(Projects.projects, id)[0];
+            });
+    }
+
+    function toggleTileView() {
+        m.tileView = (m.tileView === 'norm') ? 'min' : 'norm';
+    }
+
+    function toggleTile(tile) {
+        tile.show = !tile.show;
+    }
+
+    function findBy(key) {
+        return function (arr, val) {
+            return $.map(arr, function (p) {
+                return (p[key] !== val) ? null : p;
+            });
+        }
+    }
 }

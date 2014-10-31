b1af03bb4fba2626004c236a863b16b6d28c3406
Pavel Zhytko <pjitco@gmail.com>
2014-10-31 19:55:15 +0300

    add tooltips as example

---
+++ static/js/directives/tooltip.js
@@ -0,0 +1,16 @@
+angular
+    .module('peersay')
+    .directive('psTooltip', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element) {
+                var $el = $(element)
+                    .tooltip({
+                        delay: { "show": 600, "hide": 100 }
+                    })
+                    .click(function () {
+                        $el.tooltip('hide');
+                    });
+            }
+        };
+    });

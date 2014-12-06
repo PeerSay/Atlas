7ed8eb53eff515af28115178416cf67b60a670e2
Pavel Zhytko <pjitco@gmail.com>
2014-12-06 21:08:05 +0530

    impl client-side csv export

---
+++ static/js/directives/table-export-csv.js
@@ -0,0 +1,63 @@
+angular
+    .module('peersay')
+    .config(['$compileProvider', function($compileProvider) {
+        // allow data links
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|data):/);
+    }])
+    .directive('psExportCsv', psExportCsv);
+
+
+psExportCsv.$inject = ['$parse'];
+function psExportCsv($parse) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function(scope, element, attrs) {
+            var titles = $parse(attrs.titles)(scope.$parent);
+            var data = '';
+            var values = [];
+
+            var csv = {
+                stringify: function(str) {
+                    var res = str
+                        .replace(/^\s*/, '').replace(/\s*$/, '') // trim spaces
+                        .replace(/"/g,'""'); // replace quotes with double quotes;
+                    if (res.search(/("|,|\n)/g) >= 0) {
+                        res = '"' + res + '"'; // quote if contains special chars
+                    }
+                    return res;
+                },
+                generate: function() {
+                    //prepare data
+                    values = [titles];
+                    angular.forEach(scope.cm.criteria, function (o) {
+                        values.push([o.name, o.description, o.group, o.priority]);
+                    });
+                    //console.log('>>values', values);
+
+                    //convert to cvs
+                    data = '';
+                    angular.forEach(values, function (row) {
+                        angular.forEach(row, function (val, j) {
+                            var txt = (val === null) ? '' : val.toString();
+                            var result = csv.stringify(txt);
+                            if (j > 0) {
+                                data += ',';
+                            }
+                            data += result;
+                        });
+                        data += '\n';
+                    });
+                    //console.log('>>csv', data);
+                },
+                link: function() {
+                    // Work only in Chrome?
+                    // See this: http://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
+                    //
+                    return 'data:text/csv;charset=UTF-8,' + encodeURIComponent(data);
+                }
+            };
+            $parse(attrs.psExportCsv).assign(scope.$parent, csv);
+        }
+    };
+}

c804e9c7fbfbbbd24f30da412fac76b0e0c25089
Pavel Zhytko <pjitco@gmail.com>
2015-04-08 01:46:48 +0300

    make Evaluation dlg deep-linked too as #start; move scripts into separate file

--- static/index.js
+++ static/index.js
@@ -1,217 +1,220 @@
+$(function () {
+    /*-----------------------------------------------
+     Smooth-scroll to a section on anchor click
+    -------------------------------------------------*/
 
-$(document).ready(function(e) {
-	$('.with-hover-text, .regular-link').click(function(e){
-		e.stopPropagation();
-	});
-	
-	/***************
-	* = Hover text *
-	* Hover text for the last slide
-	***************/
-	$('.with-hover-text').hover(
-		function(e) {
-			$(this).css('overflow', 'visible');
-			$(this).find('.hover-text')
-				.show()
-				.css('opacity', 0)
-				.delay(200)
-				.animate(
-					{
-						paddingTop: '25px',
-						opacity: 1
-					},
-					'fast',
-					'linear'
-				);
-		},
-		function(e) {
-			var obj = $(this);
-			$(this).find('.hover-text')
-				.animate(
-					{
-						paddingTop: '0',
-						opacity: 0
-					},
-					'fast',
-					'linear',
-					function() {
-						$(this).hide();
-						$( obj ).css('overflow', 'hidden');
-					}
-				);
-		}
-	);
-	
-	var img_loaded = 0;
-	var j_images = [];
-	
-	/*************************
-	* = Controls active menu *
-	* Hover text for the last slide
-	*************************/
-	$('#slide-3 img').each(function(index, element) {
-		var time = new Date().getTime();
-		var oldHref = $(this).attr('src');
-		var myImg = $('<img />').attr('src', oldHref + '?' + time );
-		
-		myImg.load(function(e) {
-			img_loaded += 1;;
-			if ( img_loaded == $('#slide-3 img').length ) {
-				$(function() {
-					var pause = 10;
-					$(document).scroll(function(e) {
-						delay(function() {
-							
-							var tops = [];
-							
-							$('.story').each(function(index, element) {
-								tops.push( $(element).offset().top - 200 );
-							});
-				
-							var scroll_top = $(this).scrollTop();
-							
-							var lis = $('.nav > li');
-							
-							for ( var i=tops.length-1; i>=0; i-- ) {
-								if ( scroll_top >= tops[i] ) {
-									menu_focus( lis[i], i+1 );
-									break;
-								}
-							}
-						},
-						pause);
-					});
-					$(document).scroll();
-				});
-			}
-		});
-	});
-	
-});
+    $('a.page-scroll').on('click', function (event) {
+        var menuOffsetTop = 80;
+        var $anchor = $(this);
+        var uri = $anchor.attr('href');
+        uri = (uri !== '/') ? uri : '';
+        var newScrollTop = uri ? $(uri).offset().top - menuOffsetTop : 0;
 
-var delay = (function(){
-	var timer = 0;
-	return function(callback, ms){
-		clearTimeout (timer);
-		timer = setTimeout(callback, ms);
-	};
-})();
+        $('html, body').stop().animate({scrollTop: newScrollTop}, 800, function () {
+            // XXX - this causes scroll in FF, which hides part of the header - #59
+            //window.location.hash = uri;
+        });
 
-function menu_focus( element, i ) {
-	if ( $(element).hasClass('active') ) {
-		if ( i == 6 ) {
-			if ( $('.navbar').hasClass('inv') == false )
-				return;
-		} else {
-			return;
-		}
-	}
-
-	if ( i == 1 || i == 6 )
-		$('.navbar').removeClass('inv');
-	else
-		$('.navbar').addClass('inv');
-	
-	$('.nav > li').removeClass('active');
-	$(element).addClass('active');
-	
-	var icon = $(element).find('.fa');
-	
-	var left_pos = icon.offset().left - $('.nav').offset().left;
-	var el_width = icon.width() + $(element).find('.text').width() + 10;
-	
-	$('.active-menu').stop(false, false).animate(
-		{
-			left: left_pos,
-			width: el_width
-		},
-		1500,
-		'easeInOutQuart'
-	);
-}
-
-/*************
-* = Parallax *
-*************/
-jQuery(document).ready(function ($) {
-	//Cache some variables
-	var links = $('.nav').find('li');
-	slide = $('.slide');
-	button = $('.button');
-	mywindow = $(window);
-	htmlbody = $('html,body');
-	
-	//Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
-	//easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
-	function goToByScroll(dataslide) {
-		var offset_top = ( dataslide == 1 ) ? '0px' : $('.slide[data-slide="' + dataslide + '"]').offset().top;
-		
-		htmlbody.stop(false, false).animate({
-			scrollTop: offset_top
-		}, 1500, 'easeInOutQuart');
-	}
-	
-	//When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
-	links.click(function (e) {
-		e.preventDefault();
-		dataslide = $(this).attr('data-slide');
-		goToByScroll(dataslide);
-		//$(".nav-collapse").collapse('hide');
-	});
-	
-	//When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
-	$('.navigation-slide').click(function (e) {
-		e.preventDefault();
-		dataslide = $(this).attr('data-slide');
-		goToByScroll(dataslide);
-		//$(".nav-collapse").collapse('hide');
-	});
-});
-
-/***************
-* = Menu hover *
-***************/
-jQuery(document).ready(function ($) {
-	//Cache some variables
-	var menu_item = $('.nav').find('li');
-	
-	menu_item.hover(
-		function(e) {
-			var icon = $(this).find('.fa');
-			
-			var left_pos = icon.offset().left - $('.nav').offset().left;
-			var el_width = icon.width() + $(this).find('.text').width() + 10;
-			
-			var hover_bar = $('<div class="active-menu special-active-menu"></div>')
-				.css('left', left_pos)
-				.css('width', el_width)
-				.attr('id', 'special-active-menu-' + $(this).data('slide') );
-			
-			$('.active-menu').after( hover_bar );
-		},
-		function(e) {
-			$('.special-active-menu').remove();
-		}
-	);
-});
-
-/***********************
- * = Show/Hide navbar  *
- ***********************/
-jQuery(document).ready(function ($) {
-    $('#navigation a, #fixedbar a').on('click', function(e) {
-        e.preventDefault();
+        // Prevent location change
+        event.preventDefault();
     });
 
-    $(window).on('scroll',function() {
-        var scrolltop = $(this).scrollTop();
 
-        if(scrolltop >= 215) {
-            $('#fixedbar').fadeIn(250);
-        }
+    /*-----------------------------------------------
+     Circle Figure control
+     -------------------------------------------------*/
 
-        else if(scrolltop <= 210) {
-            $('#fixedbar').fadeOut(250);
-        }
+    $('.icon-lg')
+        .hover(toggleContentFn(true), toggleContentFn(false));
+
+    function toggleContentFn(on) {
+        return function () {
+            var $target = $($(this).attr('data-target'));
+            $target.toggleClass('active', on);
+        };
+    }
+
+
+    /*-----------------------------------------------
+     Modal: Slider
+     -------------------------------------------------*/
+
+    var $slider = $('#modal-slides').on('slide.bs.carousel', function (evt) {
+        toggleSliderControl(evt.direction);
     });
+    var $controls = $slider.find('.carousel-control');
+    toggleSliderControl('right');
+
+    function toggleSliderControl(direction) {
+        var oppositeClass = {
+            left: '.right',
+            right: '.left'
+        };
+        $controls.show();
+        $controls.filter(oppositeClass[direction]).hide();
+    }
+
+
+    /*-----------------------------------------------
+     Modal: Forms
+     -------------------------------------------------*/
+
+    var $modal = $('#modal-eval')
+        .modal({
+            backdrop: 'static',
+            show: false
+        })
+        .on('show.bs.modal', initForm);
+    var $form = $('#reg-form').submit(submitForm);
+    var $submitBtn = $modal.find('.js-submit')
+        .click(function () {
+            $form.submit();
+        });
+    var $email = $('#ev-email').keyup(function () {
+        toggleFormWarning(false);
+    });
+    var $pageEmail = $('#signup-email');
+
+    function initForm() {
+        $email.val($pageEmail.val().trim());
+
+        toggleThanksPage(false);
+    }
+
+    function updatePage() {
+        $pageEmail.val($email.val());
+    }
+
+    function submitForm() {
+        if (!validateForm()) {
+            toggleFormWarning(true);
+            return false;
+        }
+
+        var path = "/api/waiting-users";
+        var data = getFormData();
+        console.log('>> POST: ', data);
+
+        $.ajax({
+            url: path,
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (res) {
+                console.log('>> Response: ', res);
+                toggleThanksPage(true, res.email);
+                updatePage();
+            },
+            error: function () {
+                //TODO
+            }
+        });
+
+        return false;
+    }
+
+    function getFormData() {
+        var arr = $form.serializeArray();
+        var result = {};
+
+        $.each(arr, function (i, o) {
+            result[o.name] = o.value;
+        });
+        return result;
+    }
+
+    function validateForm() {
+        return validateEmail($email.val());
+    }
+
+    function validateEmail(val) {
+        val = val.trim();
+        if (!val) { return false; } // required!
+
+        var simpleEmailRe = /\S+@\S+\.\S+/;
+        return simpleEmailRe.test(val);
+    }
+
+    function toggleFormWarning(on) {
+        var $group = $email.closest('.form-group');
+        if (on) {
+            $slider.carousel(0);
+            $group.addClass('has-error');
+            $email.get(0).focus();
+        }
+        else {
+            $group.removeClass('has-error');
+        }
+    }
+
+    function toggleThanksPage(on, email) {
+        var $page = $modal.find('.thanks-page');
+
+        $submitBtn.toggle(!on);
+        $modal.find('.carousel').toggle(!on);
+
+        $page.toggle(on);
+        if (on) {
+            $page.find('.email-subst').text(email);
+        }
+    }
+
+    /*-----------------------------------------------
+     Deep-linking for Modals
+     -------------------------------------------------*/
+
+    $('.modal.js-deep-link')
+        .on('shown.bs.modal', toggleLocationHashFn(true))
+        .on('hidden.bs.modal', toggleLocationHashFn(false));
+
+    $(window).on('hashchange', showDialogOnHashChange);
+    showDialogOnHashChange(); // hashchange not triggered on page load
+
+    function showDialogOnHashChange() {
+        var $modal = $(window.location.hash);
+        var $openModal = $('.modal.in');
+        var hasOpenModal = !!$openModal.length;
+
+        if (!$modal.length) {
+            // No modals with such hash exists - close modal if open
+            $openModal.modal('hide');
+            return;
+        }
+
+        if ($modal.is($openModal)) {
+            // Same dialog
+            return;
+        }
+
+        // If some dialog is open already (which means it is a link click from that dialog),
+        // then we reload the page to let dialog go, because show/close dialog works badly in Bootstrap.
+        if (hasOpenModal) {
+            window.location.reload(true);
+        }
+        else {
+            $modal.modal('show');
+        }
+    }
+
+    function toggleLocationHashFn(on) {
+        return function () {
+            var $modal = $(this);
+            var id = $modal.get(0).id;
+            var hash = '#' + id;
+
+            toggleLocationHash(on, hash);
+        };
+    }
+
+    function toggleLocationHash(on, hash) {
+        var same = (window.location.hash === hash);
+        if (same === !!on) { return; }
+
+        if (on) {
+            window.location.hash = hash;
+        } else {
+            history.pushState("", document.title, window.location.pathname);
+        }
+    }
 });

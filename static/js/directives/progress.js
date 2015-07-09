c492a67d09f823a29bc6c36da372d9056e74b9aa
Pavel Zhytko <pjitco@gmail.com>
2015-07-09 19:03:45 +0300

    fix #114 - show colored progress-bar for popularity + tooltip

---
+++ static/js/directives/progress.js
@@ -0,0 +1,40 @@
+angular
+    .module('PeerSay')
+    .directive('psProgress', psProgress);
+
+function psProgress() {
+    return {
+        restrict: 'E',
+        scope: {
+            value: '='
+        },
+        replace: true,
+        template: [
+            '<div class="progress">',
+                '<div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="{{:: value }}">',
+                    '<span class="sr-only">{{:: value }}%</span>',
+                '</div>',
+            '</div>'
+        ].join(''),
+        link: function (scope, element) {
+            var $el = $(element).find('.progress-bar');
+
+            updateStyle();
+
+            function updateStyle() {
+                $el.css({
+                    width: scope.value + '%',
+                    backgroundColor: getBgColor(scope.value)
+                })
+            }
+
+            function getBgColor(val) {
+                var colorTpl = 'hsl($$, 90%, 60%)';
+                var hueStart = 10, hueStop = 100;
+                var hue = hueStart + Math.floor((hueStop - hueStart) * val / 100);
+                var color = colorTpl.replace('$$', hue);
+                return color;
+            }
+        }
+    };
+}

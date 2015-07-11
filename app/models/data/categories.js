024370e205f2e3d10d1b35f7cba11d0aa44b4236
Tal Ben-David <tal.bendavid@peer-say.com>
2015-07-12 00:29:05 +0300

    Added the category 'Contact & Call Center' and 7 products in the category.

--- app/models/data/categories.js
+++ app/models/data/categories.js
@@ -5,7 +5,8 @@ var categories = [
     {_id: '559741cc3dc107cb754537ee', name: 'IaaS (Infrastructure as a Service)', domain: 'Cloud', popularity: 1},
     {_id: '559741ce3dc107cb754537ef', name: 'Cloud File Sharing', domain: 'Cloud', popularity: 1},
     {_id: '559742253dc107cb754537f0', name: 'Payment Solution', domain: 'eCommerce', popularity: 1},
-    {_id: '5597422e3dc107cb754537f1', name: 'eCommerce Platform', domain: 'eCommerce', popularity: 1}
+    {_id: '5597422e3dc107cb754537f1', name: 'eCommerce Platform', domain: 'eCommerce', popularity: 1},
+    {_id: '559fc11ecb85a7644491afab', name: 'Contact & Call Center', domain: 'Customer Service', popularity: 1}
 ];
 
 module.exports = {
